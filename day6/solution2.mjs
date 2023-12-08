442ed397fba960008e6af65502117aa51585444e
Jacob Stern <jacob.stern@outlook.com>
2023-12-07 21:52:29 -0800

    Add day 6 (brute force solution)

---
+++ day6/solution2.mjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { readFile } from 'node:fs/promises';
+import { argv } from 'node:process';
+
+function numValidOperations(time, distance) {
+  let n = 0;
+  for (let holdDuration = 0; holdDuration <= time; holdDuration++) {
+    const remaining = time - holdDuration;
+    if (remaining * holdDuration > distance) {
+      n++;
+    }
+  }
+  return n;
+}
+
+const DIGITS_REGEX = /(?<digits>\d+)/g;
+
+const raceTimes = await readFile(argv[2] || './input', {
+  encoding: 'utf8',
+});
+const [times, distances] = raceTimes.split('\n');
+const timeDigits = Array.from(times.matchAll(DIGITS_REGEX))
+  .map((match) => match.groups.digits)
+  .join('');
+const distanceDigits = Array.from(distances.matchAll(DIGITS_REGEX))
+  .map((match) => match.groups.digits)
+  .join('');
+
+console.log(numValidOperations(+timeDigits, +distanceDigits));
